4b0515f30b75a9ac53070ec18db33a457efd9e9f
Kristofer Hjelmtorp <kristofer.hjelmtorp@mailbox.org>
2023-01-13 22:12:42 +0100

    Emacs-master.scm

---
+++ .dir-locals.el
@@ -0,0 +1,22 @@
+((nil
+  (fill-column . 78)
+  (tab-width   .  8)
+  (sentence-end-double-space . t)
+  (compile-command . "guix build -L"))
+
+ (scheme-mode
+  (indent-tabs-mode . nil)
+
+  ;; This notably allows '(' in Paredit to not insert a space when the
+  ;; preceding symbol is one of these.
+  (eval . (modify-syntax-entry ?~ "'"))
+  (eval . (modify-syntax-entry ?$ "'"))
+  (eval . (modify-syntax-entry ?+ "'"))
+
+  (eval . (with-eval-after-load 'geiser-guile
+            (let ((root-dir
+                   (file-name-directory
+                    (locate-dominating-file default-directory ".dir-locals.el"))))
+              (unless (member root-dir geiser-guile-load-path)
+                (setq-local geiser-guile-load-path
+                            (cons root-dir geiser-guile-load-path))))))))
---
+++ trisk/packages/emacs-master.scm
@@ -0,0 +1,45 @@
+(define-module (trisk packages emacs)
+  #:use-module (gnu packages)   ;; search-patches
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages webkit)
+  #:use-module (gnu packages text-editors) ;; tree-sitter
+  #:use-module (gnu packages sqlite)
+  #:use-module (gnu packages autotools)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module (ice-9 regex))
+
+(define-public trisk-emacs-master
+  (let ((commit "3fcbb865853d7c364cf7c2986ead9e258f449f97")
+        (revision "2"))
+    (package
+      (inherit emacs)
+      (name "emacs-master")
+      (version (git-version "29.0.50" revision commit))
+      (source
+       (origin
+         (inherit (package-source emacs))
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/emacs-mirror/emacs.git")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (patches (search-patches "emacs-exec-path.patch"
+                                  "emacs-fix-scheme-indent-function.patch"
+                                  "emacs-native-comp-driver-options.patch"))
+         (sha256
+          (base32
+           "0dyfv1cbi5c10wvh22vicaxl2bj8kh7im30122zln3xx61fpwg50"))))
+      (arguments
+       (substitute-keyword-arguments (package-arguments emacs)
+         ((#:configure-flags flags #~'())
+          #~(cons* "--with-tree-sitter" "--with-xwidgets" #$flags))))
+      (inputs
+       (modify-inputs (package-inputs emacs)
+         (prepend sqlite webkitgtk-with-libsoup2 tree-sitter)))
+      (native-inputs
+       (modify-inputs (package-native-inputs emacs)
+         (prepend autoconf))))))
+
