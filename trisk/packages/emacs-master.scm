ac264cab2671f8cc028dcae0263f8cc9283725e6
Kristofer Hjelmtorp <kristofer.hjelmtorp@mailbox.org>
2023-04-24 19:29:36 +0200

    Hopefully fixed emacs build!

--- trisk/packages/emacs-master.scm
+++ trisk/packages/emacs-master.scm
@@ -15,31 +15,31 @@
   (let ((commit "2e85ac2b270700608776ba28ecf41fc8d184db12")
         (revision "2.2"))
     (package
-      (inherit emacs)
+      (inherit emacs-next)
       (name "emacs-master")
       (version (git-version "30.0.50" revision commit))
       (source
        (origin
-         (inherit (package-source emacs))
+         (inherit (package-source emacs-next))
          (method git-fetch)
          (uri (git-reference
                (url "https://github.com/emacs-mirror/emacs.git")
                (commit commit)))
          (file-name (git-file-name name version))
          (patches (search-patches "emacs-exec-path.patch"
-                                  "emacs-fix-scheme-indent-function.patch"
-                                  "emacs-native-comp-driver-options.patch"))
+                                "emacs-fix-scheme-indent-function.patch"))
          (sha256
           (base32
            "0w4vzwiadg2czr8wd9dd3x0axb0nj6yzr6b82wjzpx0iwwzk60rf"))))
       (arguments
-       (substitute-keyword-arguments (package-arguments emacs)
+       (substitute-keyword-arguments (package-arguments emacs-next)
          ((#:configure-flags flags #~'())
           #~(cons* "--with-tree-sitter" "--with-xwidgets" #$flags))))
       (inputs
-       (modify-inputs (package-inputs emacs)
+       (modify-inputs (package-inputs emacs-next)
          (prepend sqlite webkitgtk-with-libsoup2 tree-sitter)))
       (native-inputs
-       (modify-inputs (package-native-inputs emacs)
-         (prepend autoconf))))))
-
+       (modify-inputs (package-native-inputs emacs-next)
+         (prepend autoconf)))
+      (synopsis "Emacs text editor, built from latest Git.")
+      (description "The one and true editor!"))))
