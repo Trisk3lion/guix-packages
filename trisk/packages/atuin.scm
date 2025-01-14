ab9db6fdb725bab0af7c4e8babed0a6a703eab09
Kristofer Hjelmtorp <kristofer@hjelmtorp.se>
2025-01-14 09:14:35 +0100

    Mer omflytt

--- trisk/packages/atuin.scm
+++ trisk/packages/atuin.scm
@@ -21,6 +21,7 @@
   #:use-module (gnu packages crates-windows)
   #:use-module (gnu packages crates-shell)
   #:use-module (gnu packages crates-compression)
+  #:use-module (gnu packages crates-database)
   #:use-module (gnu packages shells)
   #:use-module (gnu packages protobuf)
   #:use-module ((guix licenses) #:prefix license:)
