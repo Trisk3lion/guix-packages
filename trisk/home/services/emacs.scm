3670c8be1a87e1f499f6384f315b8ff8d3a6579e
Kristofer Hjelmtorp <kristofer.hjelmtorp@mailbox.org>
2025-02-11 21:15:57 +0100

    Added emacs home service

--- trisk/home/services/emacs.scm
+++ trisk/home/services/emacs.scm
@@ -1,87 +1,184 @@
 (define-module (trisk home services emacs)
-  #:use-module ((gnu services configuration)
-                #:select (serialize-package define-configuration
-                          define-configuration/no-serialization text-config?
-                          serialize-text-config maybe-value-set?))
-  #:use-module ((gnu packages emacs)
-                #:select (emacs))
-  #:use-module ((gnu home services)
-                #:select (service-type service-extension
-                                       home-profile-service-type
-                                       home-files-service-type))
-  #:use-module ((gnu home services shepherd)
-                #:select (shepherd-service home-shepherd-service-type))
-  #:use-module ((guix gexp)
-                #:select (gexp file-append local-file mixed-text-file))
-  #:use-module ((guix packages)
-                #:select (package?))
+  #:use-module (gnu home services)
+  #:use-module (gnu home services shepherd)
+  #:autoload   (gnu packages emacs) (emacs emacs-minimal)
+  #:autoload   (gnu packages gnupg) (guile-gcrypt)
+  #:use-module (gnu services configuration)
+  #:use-module (guix derivations)
+  #:use-module (guix gexp)
+  #:use-module (guix monads)
+  #:use-module (guix profiles)
+  #:use-module (guix records)
+  #:use-module (guix search-paths)
+  #:use-module (guix store)
+  #:use-module (ice-9 match)
+  #:export (home-emacs-configuration
+            home-emacs-service-type))
 
-  #:export (home-emacs-service-type home-emacs-configuration))
+(define list-of-string?
+  (list-of string?))
 
-;;; Emacs
+(define list-of-file-likes?
+  (list-of file-like?))
 
-(define-configuration/no-serialization home-emacs-extension
-  (configurations
-   (text-config '())
-   "The configuration for the extension."))
+(define file-like-or-#f?
+  (match-lambda
+    ((or #f (? file-like?)) #t)
+    (_ #f)))
 
-(define-configuration home-emacs-configuration
-  (package
-    (package emacs)
-    "Package to use for setting Emacs")
-  (configurations
-   (text-config '())
-   "A list of other configuration files to autoload"))
+(define-configuration/no-serialization home-emacs-configuration
+  (servers
+   (list-of-string (list "server"))
+   "List of strings which name Emacs servers to run.")
+  (emacs
+   (file-like emacs)
+   "The package providing @file{/bin/emacs}.")
+  (packages
+   (list-of-file-likes '())
+   "Packages to add to the Emacs load path.")
+  (native-compile?
+   (boolean #f)
+   "Whether to enable native-compilation of Emacs packages by building them with
+@code{emacs} rather than @code{emacs-minimal}.  Has no effect if
+COMPILATION-EMACS is not set to #F.")
+  (compilation-emacs
+   (file-like-or-#f #f)
+   "The Emacs package to use to compile Emacs packages.  If #F, default to
+EMACS-MINIMAL.")
+  (init-file
+   (file-like (plain-file "init.el" ""))
+   "File-like object to use as the Emacs initialisation script.")
+  (early-init-file
+   (file-like (plain-file "early-init.el" ""))
+   "File-like object to use as the Emacs early-initialisation script.")
+  (custom-file
+   (string "custom.el")
+   "Name of a file within the Emacs configuration directory to which variables
+set by Custom will be written.")
+  (debug?
+   (boolean #f)
+   "Whether to enable debug output from Emacs."))
 
-(define (add-emacs-packages config)
-  (list (home-emacs-configuration-package config)))
+(define (emacs-for-compile config)
+  (match-record config <home-emacs-configuration> (native-compile?
+                                                   compilation-emacs)
+    (cond
+     (compilation-emacs compilation-emacs)
+     (native-compile? emacs)
+     (else emacs-minimal))))
 
-(define (home-emacs-extensions original-config extension-configs)
-  (home-emacs-configuration (inherit original-config)
-                            (configurations (apply append
-                                                   (home-emacs-configuration-configurations
-                                                    original-config)
-                                                   (map
-                                                    home-emacs-extension-configurations
-                                                    extension-configs)))))
+(define (transformed-emacs-packages config)
+  (match-record config <home-emacs-configuration> (packages)
+    (map (package-input-rewriting
+          `((,emacs-minimal . ,(emacs-for-compile config))))
+         packages)))
 
-(define (home-emacs-shepherd-service config)
-  (list (shepherd-service (documentation "Start Emacs")
-                          (provision '(emacs))
-                          (auto-start? #t)
-                          (start #~(make-forkexec-constructor (list #$(file-append
-                                                                       (home-emacs-configuration-package
-                                                                        config)
-                                                                       "/bin/emacs")
-                                                               "--fg-daemon")
-                                                              #:log-file (format
-                                                                          #f
-                                                                          "~a/.local/var/log/xbindkeys.log"
-                                                                          (getenv
-                                                                           "HOME"))))
-                          (stop #~(make-kill-destructor)))))
+(define (make-emacs-profile config)
+  (match-record config <home-emacs-configuration> (packages)
+    (profile
+     (name "emacs-profile")
+     (content (packages->manifest (cons (emacs-for-compile config)
+                                        packages))))))
 
-(define (home-emacs-config-files config)
-  `((".emacs.d/init.el" ,(mixed-text-file "init.el"
-                                          (serialize-text-config
-                                           config
-                                           (home-emacs-configuration-configurations
-                                                                  config))))))
+(define (build-emacs-profile config)
+  (with-store store
+    (run-with-store store
+      (mlet %store-monad
+          ((profile-drv (lower-object
+                         (make-emacs-profile config))))
+        (mbegin %store-monad
+          (built-derivations (list profile-drv))
+          (return (derivation-output-path
+                   (assoc-ref (derivation-outputs profile-drv)
+                              "out"))))))))
+
+(define (emacs-environment config)
+  #~(map (lambda (env-var)
+           (let ((variable (list-ref env-var 0))
+                 (separator (list-ref env-var 1))
+                 (value (list-ref env-var 2)))
+             (string-append variable "="
+                            (or (and=> (getenv variable)
+                                       (lambda (original)
+                                         (string-append original
+                                                        separator)))
+                                "")
+                            value)))
+         '(#$@(map (match-lambda
+                     ((spec . value)
+                      (match-record spec <search-path-specification>
+                          (variable separator)
+                        (list variable separator value))))
+                   (profile-search-paths
+                    (build-emacs-profile config))))))
+
+(define (home-emacs-profile-packages config)
+  (match-record config <home-emacs-configuration> (emacs)
+    (list emacs)))
+
+(define (home-emacs-shepherd-services config)
+  (match-record config <home-emacs-configuration>
+      (servers emacs custom-file debug?)
+    (map (lambda (server)
+           (shepherd-service
+            (provision (list (string->symbol
+                              (string-append "emacs-" server))))
+            (documentation
+             (string-append "Start the Emacs server called "
+                            server "."))
+            (start
+             #~(make-forkexec-constructor
+                (list #$(file-append emacs "/bin/emacs")
+                      (string-append "--fg-daemon=" #$server)
+                      "--eval"
+                      (format #f "~s"
+                              (let ((custom-file
+                                     (format #f "~a/emacs/custom.el"
+                                             (or (getenv "XDG_CONFIG_HOME")
+                                                 (format #f "~a/.config"
+                                                         (getenv "HOME"))))))
+                                `(when (file-readable-p ,custom-file)
+                                   (setq custom-file ,custom-file)
+                                   (load custom-file))))
+                      #$@(if debug?
+                             (list "--debug-init")
+                             '()))
+                #:log-file
+                (format #f "~a/log/emacs/~a.log"
+                        (or (getenv "XDG_STATE_HOME")
+                            (format #f "~a/.local/state"
+                                    (getenv "HOME")))
+                        #$server)
+                #:environment-variables
+                (append (default-environment-variables)
+                        #$(emacs-environment config))))
+            (stop
+             #~(make-forkexec-constructor
+                (list #$(file-append emacs "/bin/emacsclient")
+                      "-s" #$server "--eval" "(kill-emacs)")))))
+         servers)))
+
+(define (home-emacs-xdg-configuration-files config)
+  (match-record config <home-emacs-configuration> (early-init-file
+                                                   init-file)
+    `(("emacs/early-init.el"
+       ,(home-emacs-configuration-early-init-file config))
+      ("emacs/init.el"
+       ,(home-emacs-configuration-init-file config)))))
 
 (define home-emacs-service-type
   (service-type
    (name 'home-emacs)
-   (extensions (list
-                (service-extension
-                 home-profile-service-type
-                 add-emacs-packages)
-                (service-extension
-                 home-shepherd-service-type
-                 home-emacs-shepherd-service)
-                (service-extension
-                 home-files-service-type
-                 home-emacs-config-files)))
-   (compose identity)
-   (extend home-emacs-extensions)
+   (extensions
+    (list (service-extension
+           home-profile-service-type
+           home-emacs-profile-packages)
+          (service-extension
+           home-shepherd-service-type
+           home-emacs-shepherd-services)
+          (service-extension
+           home-xdg-configuration-files-service-type
+           home-emacs-xdg-configuration-files)))
    (default-value (home-emacs-configuration))
-   (description "Install and configure Emacs.")))
+   (description
+    "Configure and run the GNU Emacs extensible text editor.")))
