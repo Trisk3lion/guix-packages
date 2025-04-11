608ceb4b8b3d4bc2c03b51f8b13a390fedc962fc
Kristofer Hjelmtorp <kristofer@hjelmtorp.se>
2025-04-11 21:48:15 +0200

    fwupd service

---
+++ trisk/services/fwupd.scm
@@ -0,0 +1,48 @@
+(define-module (trisk services fwupd)
+  #:use-module (gnu packages firmware)
+  #:use-module (gnu services)
+  #:use-module (gnu services configuration)
+  #:use-module (gnu services dbus)
+  #:use-module (gnu services shepherd)
+  #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (srfi srfi-1)
+  #:export (fwupd-configuration
+            fwupd-configuration?
+            fwupd-configuration-fields
+            fwupd-configuration-fwupd
+
+            fwupd-service-type))
+
+(define-configuration/no-serialization fwupd-configuration
+  (fwupd
+   (package fwupd)
+   "The fwupd package that will be installed in the system profile."))
+
+(define (fwupd-shepherd-services config)
+  (let ((fwupd
+         (fwupd-configuration-fwupd config)))
+    (list
+     (shepherd-service (provision '(fwupd))
+                       (requirement '(user-processes))
+                       (documentation "Run the fwupd daemon.")
+                       (start #~(make-forkexec-constructor
+                                 (list
+                                  #$(file-append fwupd "/libexec/fwupd/fwupd"))))
+                       (stop #~(make-kill-destructor))))))
+
+(define fwupd-service-type
+  (service-type (name 'fwupd)
+                (extensions
+                 (list
+                  (service-extension shepherd-root-service-type
+                                     fwupd-shepherd-services)
+                  (service-extension profile-service-type
+                                     (compose list fwupd-configuration-fwupd))
+                  (service-extension dbus-root-service-type
+                                     (compose list fwupd-configuration-fwupd))
+                  (service-extension polkit-service-type
+                                     (compose list fwupd-configuration-fwupd))))
+                (default-value (fwupd-configuration))
+                (description
+                 "This service configures fwupd on the Guix System.")))
