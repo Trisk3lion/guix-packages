ccb1b028036535a520f52ac03f5aa9b8089741b5
Kristofer Hjelmtorp <kristofer@hjelmtorp.se>
2025-11-29 07:41:21 +0100

    emacs-master

--- trisk/packages/emacs-master.scm
+++ trisk/packages/emacs-master.scm
@@ -1,22 +1,225 @@
 (define-module (trisk packages emacs-master)
   #:use-module (gnu packages)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages check)
   #:use-module (gnu packages emacs)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages texinfo)
+  #:use-module (gnu packages xorg)
+  #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (guix gexp)
-  #:use-module (guix packages)
   #:use-module (guix git-download)
-  #:use-module (guix download)
-  #:use-module (ice-9 regex)
-  #:use-module (rnrs lists)
-  ;; #:use-module (trisk packages)
-  )
+  #:use-module (guix download))
 
-(define emacs-git-commit
-  "103a2fdf18d99c872e4adb01a15a8300164acd66")
-(define emacs-git-hash
-  "19w5nrnhj5b26l2pjkq9adsp1kpixq8d334mvsnr1j5by8229i0c")
-(define emacs-git-time
-  "1712773724")
+(define emacs-master-commit "11b68c6223d154b4c85e8feec63581dcf7c76a3e")
+(define emacs-master-igc-commit "f782dd29fb31c2f42429be1e9db4d95b85beb39f")
+
+;; Returns the first seven characters of a commit.
+(define (shorthand-commit commit)
+  (string-drop-right commit 33))
+
+(define emacs-master-hash "13an7b9gkcd2b25nm66xcqvpps54fbhwi16315gd1n4gfa4b16pb")
+(define emacs-master-igc-hash "1q5kj8j9apy2lgpgfpvw3sda9bgal0z8z5gbqflqlj9764d6g74w")
+
+(define patches-path "trisk/packages/patches/")
+
+(define (from-patches patch)
+  (string-append patches-path patch))
+
+;; From emacs.scm.
+(define* (emacs-ert-selector excluded-tests #:key run-nativecomp run-expensive run-unstable)
+  "Create an ERT selector that excludes tests."
+  (string-append
+   "(not (or "
+   (if run-nativecomp
+       ""
+       "(tag :nativecomp) ")
+   (if run-expensive
+       ""
+       "(tag :expensive-test) ")
+   (if run-unstable
+       ""
+       "(tag :unstable) ")
+   (string-join
+    (map
+     (lambda (test)
+       (string-append "\\\"" test "\\\""))
+     excluded-tests))
+   "))"))
+
+(define emacs-master-selector
+  (emacs-ert-selector
+   '("bytecomp--fun-value-as-head"
+     "esh-util-test/path/get-remote"
+     "esh-var-test/path-var/preserve-across-hosts"
+     "ffap-tests--c-path"
+     "find-func-tests--locate-macro-generated-symbols"
+     "grep-tests--rgrep-abbreviate-properties-darwin"
+     "grep-tests--rgrep-abbreviate-properties-gnu-linux"
+     "grep-tests--rgrep-abbreviate-properties-windows-nt-dos-semantics"
+     "grep-tests--rgrep-abbreviate-properties-windows-nt-sh-semantics"
+     "info-xref-test-makeinfo"
+     "man-tests-find-header-file"
+     "tab-bar-tests-quit-restore-window"
+     "tramp-test48-remote-load-path"
+     "tramp-test49-remote-load-path"
+     "tramp-test50-remote-load-path"
+     "lua-ts-test-auto-close-block-comments"
+     ;; Tests that are failing for now.
+     "python-hideshow-hide-levels-1"
+     "python-hideshow-hide-levels-2"
+     ;; For emacs-master-igc.
+     "module--test-assertions--call-emacs-from-gc"
+     "process-tests/fd-setsize-no-crash/make-process"
+     "set-commit-limit-test")))
+
+(define-public emacs-master-minimal
+  (package
+    (inherit emacs-next-minimal)
+    (name "emacs-master-minimal")
+    (version (git-version "31.0.50" "1" (shorthand-commit emacs-master-commit)))
+    (source
+     (origin
+       (inherit (package-source emacs-next-minimal))
+       (uri (git-reference
+              (url "https://github.com/emacs-mirror/emacs.git")
+              (commit emacs-master-commit)))
+       (sha256
+        (base32 emacs-master-hash))
+       (patches (search-patches (from-patches "emacs-master-disable-jit-compilation.patch")
+                                (from-patches "emacs-master-exec-path.patch")
+                                (from-patches "emacs-master-native-comp-fix-filenames.patch")
+                                "emacs-fix-scheme-indent-function.patch"
+                                "emacs-native-comp-driver-options.patch"
+                                "emacs-native-comp-pin-packages.patch"
+                                "emacs-pgtk-super-key-fix.patch"))
+       (snippet
+        '(for-each delete-file-recursively
+                   '("test/lisp/textmodes/ispell-resources"
+                     "test/lisp/textmodes/ispell-tests")))))
+    (arguments
+     (substitute-keyword-arguments (package-arguments emacs-next-minimal)
+       ((#:make-flags flags #~'())
+        #~(list (string-append "SELECTOR=" #$emacs-master-selector)
+                (let ((release-date "2025-08-14 05:04:03"))
+                  (string-append "RUN_TEMACS= "
+                                 #$libfaketime
+                                 "/bin/faketime -m -f '" release-date "'"
+                                 " ./temacs"))))))
+    ;; "python-shell--convert-file-name-to-send-1" test requires python3.
+    (native-inputs
+     (modify-inputs (package-native-inputs emacs-next-minimal)
+       (append python-3)))))
+
+(define (masterize-name emacs)
+  (when (eq? (package-name emacs) "emacs-next")
+    (string-append "emacs-master"
+                   (string-drop (package-name emacs)
+                                (string-length "emacs-next"))))
+  (string-append "emacs-master"
+                 (string-drop (package-name emacs)
+                              (string-length "emacs"))))
+
+(define* (emacs->emacs-master emacs
+                              #:optional name
+                              #:key
+                              ;; Source and version come from
+                              ;; emacs-master-minimal to keep the package updated.
+                              (source (package-source emacs-master-minimal))
+                              (version (package-version emacs-master-minimal))
+                              ;; But the arguments and inputs come from the
+                              ;; originals to better fit their quirks.
+                              (arguments
+                               (substitute-keyword-arguments (package-arguments emacs)
+                                 ;; But only in part. We need to use the text
+                                 ;; excluder from here!
+                                 ((#:make-flags flags #~'())
+                                  #~(list (string-append "SELECTOR=" #$emacs-master-selector)
+                                          (let ((release-date "2025-08-14 05:04:03"))
+                                            (string-append "RUN_TEMACS= "
+                                                           #$libfaketime
+                                                           "/bin/faketime -m -f '" release-date "'"
+                                                           " ./temacs"))))))
+                              (inputs (package-inputs emacs))
+                              ;; "python-shell--convert-file-name-to-send-1" test requires python3.
+                              (native-inputs
+                               (modify-inputs (package-native-inputs emacs)
+                                 (append python-3))))
+  (package
+    (inherit emacs)
+    (name (or name (masterize-name emacs)))
+    (version version)
+    (source source)
+    (arguments arguments)
+    (inputs inputs)
+    (native-inputs native-inputs)))
+
+(define-public emacs-master (emacs->emacs-master emacs))
+
+;; No graphical elements.
+(define-public emacs-master-no-x (emacs->emacs-master emacs-no-x))
+(define-public emacs-master-no-x-toolkit (emacs->emacs-master emacs-no-x-toolkit))
+
+;; PGTK
+(define-public emacs-master-pgtk
+  (emacs->emacs-master emacs-pgtk))
+
+;; Motif
+(define-public emacs-master-motif
+  (emacs->emacs-master emacs-motif))
+
+;; Lucid
+(define-public emacs-master-lucid
+  (emacs->emacs-master emacs-lucid))
+
+;; New Garbage Collector branch for testing.
+(define-public emacs-master-igc
+  (emacs->emacs-master
+   emacs
+   ;; Necessary, or else the package would have the wrong name.
+   "emacs-master-igc"
+   #:source
+   (origin
+     (inherit (package-source emacs-next-minimal))
+     (uri (git-reference
+            (url "https://github.com/emacs-mirror/emacs.git")
+            (commit emacs-master-igc-commit)))
+     (sha256
+      (base32 emacs-master-igc-hash))
+     ;; Patches are cherry-picked because this branch takes some time to
+     ;; catch-up with master.
+     (patches (search-patches "emacs-fix-scheme-indent-function.patch"
+                              "emacs-native-comp-pin-packages.patch"
+                              "emacs-pgtk-super-key-fix.patch"
+                              "emacs-next-disable-jit-compilation.patch"
+                              "emacs-next-exec-path.patch"
+                              "emacs-next-native-comp-fix-filenames.patch")))
+   #:version ; Different commit, different version.
+   (git-version "31.0.50" "1" (shorthand-commit emacs-master-igc-commit))
+   #:arguments
+   (substitute-keyword-arguments (package-arguments emacs)
+     ((#:configure-flags flags #~'())
+      #~(cons* "--with-mps=yes" #$flags))
+     ((#:make-flags flags #~'())
+      #~(list (string-append "SELECTOR=" #$emacs-master-selector)
+              (let ((release-date "2025-08-14 05:04:03"))
+                (string-append "RUN_TEMACS= "
+                               #$libfaketime
+                               "/bin/faketime -m -f '" release-date "'"
+                               " ./temacs")))))
+   #:inputs
+   (modify-inputs (package-inputs emacs)
+     (append (@@ (trisk packages mps) mps)))))
+(define-public emacs-master-pgtk-igc
+  (package/inherit emacs-master-igc
+    (name "emacs-master-pgtk-igc")
+    (arguments
+     (substitute-keyword-arguments (package-arguments emacs-master-igc)
+       ((#:configure-flags flags #~'())
+        #~(cons* "--with-pgtk" #$flags))))))
+
 
 ;; (define-public emacs-master-minimal
 ;;   (package
