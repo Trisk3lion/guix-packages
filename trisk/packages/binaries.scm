f09f6da20728352c9bba8e03f2e7e7e7880a2cb2
Kristofer Hjelmtorp <kristofer@hjelmtorp.se>
2026-01-10 10:01:27 +0100

    Update ntfy-bin to 2.15.0

--- trisk/packages/binaries.scm
+++ trisk/packages/binaries.scm
@@ -1,5 +1,4 @@
 (define-module (trisk packages binaries)
-  #:use-module (guix)
   #:use-module (guix packages)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages compression)
@@ -17,10 +16,10 @@
          (binary (cond
                   ((equal? system "x86_64-linux") "linux_amd64")))
          (hash (cond
-                  ((equal? system "x86_64-linux") "1vbplsq85x6s5wjqbryhc8p0qbdll6nyi6rbdrrizqxcw9l2hwk1"))))
+                  ((equal? system "x86_64-linux") "13jg1hk5z2bzw8jaw0yb3c6q8390ic0407945dw4f9d7l1im56h1"))))
     (package
       (name "ntfy")
-      (version "2.11.0")
+      (version "2.15.0")
       (source (origin
                 (method url-fetch)
                 (uri (string-append "https://github.com/binwiederhier/ntfy/releases/download/v"
@@ -43,7 +42,7 @@ phone or desktop via scripts} from any computer, @@strong{without having to sign
 up or pay any fees}.  If you'd like to run your own instance of the service, you
 can easily do so since ntfy is open source.")
       (properties
-       `(;(hidden? . #t)
+       '(;(hidden? . #t)
          (release-monitoring-url . "https://github.com/binwiederhier/ntfy/releases")
          (upstream-name . "ntfy")))
       (supported-systems '("x86_64-linux"))
@@ -58,7 +57,7 @@ can easily do so since ntfy is open source.")
                   ((equal? system "x86_64-linux") "1sbirkx918a5bnm7zsyppnbqkpaz3y1q4y3c1p2ak18p04390dv3"))))
     (package
       (name "beszel")
-      (version "0.11.1")
+      (version "0.17.0")
       (source (origin
                 (method url-fetch)
                 (uri (string-append "https://github.com/henrygd/beszel/releases/download/v"
@@ -81,10 +80,10 @@ can easily do so since ntfy is open source.")
          (binary (cond
                   ((equal? system "x86_64-linux") "linux_amd64")))
          (hash (cond
-                  ((equal? system "x86_64-linux") "0pri0985k754j1z3vmz7v42mz895p871p0l0was1jvgkvvlwmcmr"))))
+                  ((equal? system "x86_64-linux") "0vhb242qb4p77pcs5xm5s4k0hbyf7h5h28yw1k8qiq8srlaxp78j"))))
     (package
       (name "beszel-agent")
-      (version "0.11.1")
+      (version "0.17.0")
       (source (origin
                 (method url-fetch)
                 (uri (string-append "https://github.com/henrygd/beszel/releases/download/v"
@@ -107,7 +106,7 @@ can easily do so since ntfy is open source.")
 (define-public editor-code-assistant
   (package
     (name "editor-code-assistant")
-    (version "0.79.0")
+    (version "0.90.0")
     (source
      (origin
        (method url-fetch/zipbomb)
@@ -115,7 +114,7 @@ can easily do so since ntfy is open source.")
                            "/releases/download/" version
                            "/eca-native-static-linux-amd64.zip"))
        (sha256
-        (base32 "1i9zvwa6rdp20af58axbhgghyvgh9j7y5662fb8qfdwby2dqpskn"))))
+        (base32 "0qp5dwwxlyvbylkn7n10csl3naf2jqi1lxxv2fyc47b6szm0hwlk"))))
     (build-system binary-build-system)
     (arguments
      `(#:install-plan
@@ -134,65 +133,7 @@ languages.  The goal of this project is to bring great editing tools for
 Clojure/Clojurescript to all editors and programatically via its CLI and API.
 It aims to work alongside you to help you navigate, identify and fix errors,
 perform refactors and more.")
-    (license license:expat)))
-
-(define babashka-git-version
-  "1.3.189")
-(define babashka-git-hash
-  "04m7wlq2740brnpfff75005v06wr3cb751kdqrk8vfi4wz78r3i2")
-
-(define-public babashka
-  (package
-    (name "babashka")
-    (version babashka-git-version)
-    (source
-     (origin
-       (method url-fetch)
-       (uri (string-append
-             "https://github.com/babashka/babashka/releases/download/v"
-             version "/babashka-" version "-linux-amd64-static.tar.gz"))
-       (sha256
-        (base32 babashka-git-hash))))
-    (build-system copy-build-system)
-    (supported-systems '("x86_64-linux" "i686-linux"))
-    (arguments
-     `(#:install-plan '(("bb" "bin/"))))
-    (synopsis "Fast native Clojure scripting runtime")
-    (description
-     "Babashka is a native Clojure interpreter for scripting with fast startup.
-Its main goal is to leverage Clojure in places where you would be using bash otherwise.")
-    (home-page "https://babashka.org/")
     (properties
-       `(;(hidden? . #t)
-         (release-monitoring-url . "https://github.com/babashka/babashka/releases")
-         (upstream-name . "babashka")))
-    (license license:epl1.0)))
-
-(define neil-git-version
-  "0.3.65")
-(define neil-git-hash
-  "0qwxjzyisdxc61a43df1rmk1pqifankg0g51sw6hlrjyw8sk06q5")
-
-;; (define-public neil
-;;   (package
-;;     (name "neil")
-;;     (version neil-git-version)
-;;     (source
-;;      (origin
-;;        (method url-fetch)
-;;        (uri (string-append
-;;              "https://github.com/neil/neil/releases/download/"
-;;              version
-;;              "/neil-native-static-linux-amd64.zip"))
-;;        (sha256
-;;         (base32 neil-git-hash))))
-;;     (build-system copy-build-system)
-;;     (native-inputs (list babashka java))
-;;     (arguments
-;;      `(#:install-plan
-;;        '(("neil" "bin/"))))
-;;     (home-page "https://clojure.org/releases/tools")
-;;     (synopsis "CLI tools for the Clojure programming language")
-;;     (description "The Clojure command line tools can be used to start a
-;; Clojure repl, use Clojure and Java libraries, and start Clojure programs.")
-;;     (license license:epl1.0)))
+     '((release-monitoring-url . "https://github.com/editor-code-assistant/eca/releases")
+       (upstream-name . "eca")))
+    (license license:expat)))
