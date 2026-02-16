3095ff067d2e1712cd51983ee4c07e2e1b027683
Kristofer Hjelmtorp <kristofer@hjelmtorp.se>
2026-02-16 09:11:10 +0100

    Forgot a package

---
+++ trisk/packages/ai.scm
@@ -0,0 +1,53 @@
+(define-module (trisk packages ai)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages gcc)
+  #:use-module (guix download)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (nonguix build-system binary)
+  #:use-module (nonguix licenses)
+  )
+
+(define-public ollama
+  (package
+    (name "ollama")
+    (version "0.15.6")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://github.com/ollama/ollama/releases/download/v"
+             version "/ollama-linux-amd64.tar.zst"))
+       (sha256
+        (base32 "0lql27q7hl9va0wz3s0v9q0r7n6b18si821bja3cadvq1kl4wfi7"))))
+    (build-system binary-build-system)
+    (arguments
+     (list
+      #:strip-binaries? #f
+      #:validate-runpath? #f
+      #:patchelf-plan
+      #~'(("bin/ollama" ("glibc" "gcc")))
+      #:install-plan
+      #~'(("bin/ollama" "bin/"))
+      #:phases
+      #~(modify-phases %standard-phases
+          (replace 'unpack
+            (lambda* (#:key inputs #:allow-other-keys)
+              (invoke "tar" "--use-compress-program=zstd" "-xf"
+                      (assoc-ref inputs "source")))))))
+    (native-inputs
+     (list zstd))
+    (inputs
+     (list glibc
+           `(,gcc "lib")))
+    (supported-systems '("x86_64-linux"))
+    (home-page "https://ollama.com")
+    (synopsis "Run large language models locally")
+    (description
+     "Ollama allows you to run large language models locally.
+It provides a simple API for creating, running and managing models,
+as well as a library of pre-built models that can be easily used.")
+    (license license:expat)))
