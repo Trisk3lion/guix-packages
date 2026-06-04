b5100f0c417d8f14a6d839fe157bbe9f344d2758
Kristofer Hjelmtorp <kristofer@hjelmtorp.se>
2026-06-04 21:43:21 +0200

    Updates

--- trisk/packages/wm.scm
+++ trisk/packages/wm.scm
@@ -9,50 +9,37 @@
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages web)
   #:use-module (gnu packages c)
-  #:use-module (gnu packages wm))
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages cmake))
 
 (define-public vali
-  (package
-    (name "vali")
-    (version "0.1.1")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                     (url "https://gitlab.freedesktop.org/emersion/vali")
-                     (commit (string-append "v" version))))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32 "0xycciy9f2iz41w8vrj0w0pan75x3hfgb15g67262kw7ga5lkfxv"))))
-    (build-system meson-build-system)
-    (native-inputs (list pkg-config))
-    (inputs (list json-c aml))
-    (home-page "https://gitlab.freedesktop.org/emersion/vali")
-    (synopsis "")
-    (description "")
-    (license license:expat)))
+  (let ((commit "cfc9abf485d8ac2ed4992fdd11aab4d7fd87eedf")
+        (revision "1"))
+    (package
+      (name "vali")
+      (version "0.1.1")
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                       (url "https://gitlab.freedesktop.org/emersion/vali")
+                       (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32 "129c5fkb96kgkfdnbx1z88afafy8lrb1gdkckb9zhw25k3nyq72g"))))
+      (build-system meson-build-system)
+      (native-inputs (list pkg-config))
+      (inputs (list json-c aml))
+      (propagated-inputs (list json-c aml))
+      (home-page "https://gitlab.freedesktop.org/emersion/vali")
+      (synopsis "")
+      (description "")
+      (license license:expat))))
 
 (define-public kanshi-vali
   (package
     (inherit kanshi)
     (name "kanshi-vali")
     (arguments
-     (list
-      #:parallel-build? #f
-      #:configure-flags #~(list "-Dipc=enabled")))
+     (list #:configure-flags #~(list "-Dipc=enabled")))
     (inputs (modify-inputs inputs
               (replace "libvarlink" vali)))))
-
-(define-public kanshi-1.8.0
-  (package
-    (inherit kanshi)
-    (name "kanshi-1.8.0")
-    (version "1.8.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-              (url "https://gitlab.freedesktop.org/emersion/kanshi")
-              (commit (string-append "v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "1aw43fdrrgywghy59ahfxwq0613ydkpl2j6p042c0iwqv1b6fhgp"))))))
